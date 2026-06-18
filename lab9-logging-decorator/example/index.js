1b3a7d44d8f9bb2b6ada95b9b77c3cf3a4db36cc
lilianasemeniuk <lilianasemeniuk7@gmail.com>
2026-06-18 23:27:09 +0300

    feat: support async functions in logging decorator

--- lab9-logging-decorator/example/index.js
+++ lab9-logging-decorator/example/index.js
@@ -8,3 +8,12 @@ const loggedAdd = log(add, "INFO");
 
 loggedAdd(2, 3);
 loggedAdd(5, 7);
+
+async function loadUser(id) {
+    await new Promise((resolve) => setTimeout(resolve,500));
+    return { id, name: "Liliana" };
+}
+
+const loggedLoad = log(loadUser, "INFO");
+
+loggedLoad(1);
--- lab9-logging-decorator/lib/logger.js
+++ lab9-logging-decorator/lib/logger.js
@@ -1,11 +1,11 @@
 const LEVELS = { ERROR: 0, INFO: 1, DEBUG: 2 };
 
 export function log(fn, level = 'INFO') {
-    return function (...args) {
+    return async function (...args) {
         const timestamp = new Date().toISOString();
         console.log(`[${timestamp}] [${level}] ${fn.name} (${JSON.stringify(args)})`);
 
-        const result = fn(...args);
+        const result = await fn(...args);
 
         console.log(`[${timestamp}] [${level}] ${fn.name} => ${JSON.stringify(result)}`);
         return result;
